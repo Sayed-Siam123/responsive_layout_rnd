aaf9676d2f3b81aae3335b2bd73091436269092f
Taki Uddin <msiam18@gmail.com>
2022-09-18 15:34:03 +0600

    Data table added for rnd

--- lib/modules/spalsh/Splash/SplashMobile.dart
+++ lib/modules/spalsh/Splash/SplashMobile.dart
@@ -1,3 +1,4 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -15,11 +16,72 @@ class SplashMobilePortrait extends GetView<SplashLogic> {
     Get.find<InternetCheckerHelperLogic>();
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Text("Mobile Porttrait"),
-          ],
-        ),
+        body: DataTable2(
+            columnSpacing: 12,
+            horizontalMargin: 12,
+            minWidth: 1500,
+            border: TableBorder(
+              // top: BorderSide(color: Colors.red),
+              // bottom: BorderSide(color: Colors.red),
+              // left: BorderSide(color: Colors.red),
+              // right: BorderSide(color: Colors.red),
+              horizontalInside: BorderSide(color: Colors.red),
+              verticalInside: BorderSide(color: Colors.red),
+            ),
+            dataRowColor: MaterialStateProperty.resolveWith((states) {
+              // If the button is pressed, return green, otherwise blue
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.white;
+              }
+              return Colors.white;
+            }),
+            fixedColumnsColor: Colors.blue,
+            showCheckboxColumn: true,
+            headingRowColor: MaterialStateProperty.resolveWith((states) {
+              // If the button is pressed, return green, otherwise blue
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.white;
+              }
+              return Colors.white;
+            }),
+            columns: [
+              DataColumn2(
+                label: Text('Column A'),
+                fixedWidth: 200,
+              ),
+              DataColumn2(
+                fixedWidth: 300,
+                label: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 3,),
+                    Text('Column B'),
+                    Divider(color: Colors.black),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Column B'),
+                        Text('Column B'),
+                        Text('Column B'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+            rows: List<DataRow>.generate(
+                100,
+                    (index) => DataRow(cells: [
+                  DataCell(Text('A' * (10 - index % 10))),
+                  DataCell(Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('B' * (10 - (index + 5) % 10)),
+                      Text('B' * (10 - (index + 5) % 10)),
+                      Text('B' * (10 - (index + 5) % 10)),
+                    ],
+                  )),
+                ]))),
       ),
     );
   }
