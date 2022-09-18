dbc29b13c739a5eb523ded062968068776cd507b
Taki Uddin <msiam18@gmail.com>
2022-09-18 16:00:25 +0600

    Data table fixed for implementation, now need to design json

--- lib/modules/spalsh/Splash/SplashMobile.dart
+++ lib/modules/spalsh/Splash/SplashMobile.dart
@@ -17,16 +17,17 @@ class SplashMobilePortrait extends GetView<SplashLogic> {
     return SafeArea(
       child: Scaffold(
         body: DataTable2(
-            columnSpacing: 12,
-            horizontalMargin: 12,
+            columnSpacing: 2,
+            horizontalMargin: 2,
             minWidth: 1500,
+            dividerThickness: 0,
             border: TableBorder(
-              // top: BorderSide(color: Colors.red),
-              // bottom: BorderSide(color: Colors.red),
-              // left: BorderSide(color: Colors.red),
-              // right: BorderSide(color: Colors.red),
-              horizontalInside: BorderSide(color: Colors.red),
-              verticalInside: BorderSide(color: Colors.red),
+              top: BorderSide(color: Colors.black),
+              bottom: BorderSide(color: Colors.black),
+              left: BorderSide(color: Colors.black),
+              right: BorderSide(color: Colors.black),
+              horizontalInside: BorderSide(color: Colors.black),
+              verticalInside: BorderSide(color: Colors.black),
             ),
             dataRowColor: MaterialStateProperty.resolveWith((states) {
               // If the button is pressed, return green, otherwise blue
@@ -46,7 +47,7 @@ class SplashMobilePortrait extends GetView<SplashLogic> {
             }),
             columns: [
               DataColumn2(
-                label: Text('Column A'),
+                label: Center(child: Text('Column A')),
                 fixedWidth: 200,
               ),
               DataColumn2(
@@ -56,31 +57,53 @@ class SplashMobilePortrait extends GetView<SplashLogic> {
                   children: [
                     SizedBox(height: 3,),
                     Text('Column B'),
-                    Divider(color: Colors.black),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Column B'),
-                        Text('Column B'),
-                        Text('Column B'),
-                      ],
+                    Divider(color: Colors.black87,thickness: 1,height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Column B'),
+                            VerticalDivider(color: Colors.black87,thickness: 1),
+                            Text('Column B'),
+                            VerticalDivider(color: Colors.black87,thickness: 1),
+                            Text('Column B'),
+                          ],
+                        ),
+                      ),
                     )
                   ],
                 ),
+              ),
+              DataColumn2(
+                label: Center(child: Text('Column C')),
+                fixedWidth: 200,
+              ),
+              DataColumn2(
+                label: Center(child: Text('Column D')),
+                fixedWidth: 200,
               ),
             ],
             rows: List<DataRow>.generate(
                 100,
                     (index) => DataRow(cells: [
-                  DataCell(Text('A' * (10 - index % 10))),
-                  DataCell(Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('B' * (10 - (index + 5) % 10)),
-                      Text('B' * (10 - (index + 5) % 10)),
-                      Text('B' * (10 - (index + 5) % 10)),
-                    ],
+                  DataCell(Center(child: Text('A' * (10 - index % 10)))),
+                  DataCell(Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(width: 50,child: Text('B' * (10 - (index + 5) % 10))),
+                        VerticalDivider(color: Colors.black87,thickness: 1),
+                        SizedBox(width: 50,child: Text('B' * (10 - (index + 5) % 10))),
+                        VerticalDivider(color: Colors.black87,thickness: 1),
+                        SizedBox(width: 50,child: Text('B' * (10 - (index + 5) % 10))),
+                      ],
+                    ),
                   )),
+                  DataCell(Center(child: Text('C' * (10 - index % 10)))),
+                  DataCell(Center(child: Text('D' * (10 - index % 10)))),
                 ]))),
       ),
     );
@@ -97,11 +120,95 @@ class SplashMobileLandscape extends GetView<SplashLogic> {
     Get.find<InternetCheckerHelperLogic>();
     return SafeArea(
       child: Scaffold(
-        body: Column(
-            children: [
-            Text("Mobile Landscape"),
-          ],
-        ),
+        body: DataTable2(
+            columnSpacing: 2,
+            horizontalMargin: 12,
+            minWidth: 1500,
+            dividerThickness: 0,
+            border: TableBorder(
+              top: BorderSide(color: Colors.black),
+              bottom: BorderSide(color: Colors.black),
+              left: BorderSide(color: Colors.black),
+              right: BorderSide(color: Colors.black),
+              horizontalInside: BorderSide(color: Colors.black),
+              verticalInside: BorderSide(color: Colors.black),
+            ),
+            dataRowColor: MaterialStateProperty.resolveWith((states) {
+              // If the button is pressed, return green, otherwise blue
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.white;
+              }
+              return Colors.white;
+            }),
+            fixedColumnsColor: Colors.blue,
+            showCheckboxColumn: true,
+            headingRowColor: MaterialStateProperty.resolveWith((states) {
+              // If the button is pressed, return green, otherwise blue
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.white;
+              }
+              return Colors.white;
+            }),
+            columns: [
+              DataColumn2(
+                label: Center(child: Text('Column A')),
+                fixedWidth: 200,
+              ),
+              DataColumn2(
+                fixedWidth: 300,
+                label: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 3,),
+                    Text('Column B'),
+                    Divider(color: Colors.black87,thickness: 1,height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Column B'),
+                            VerticalDivider(color: Colors.black87,thickness: 1),
+                            Text('Column B'),
+                            VerticalDivider(color: Colors.black87,thickness: 1),
+                            Text('Column B'),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              DataColumn2(
+                label: Center(child: Text('Column C')),
+                fixedWidth: 200,
+              ),
+              DataColumn2(
+                label: Center(child: Text('Column D')),
+                fixedWidth: 200,
+              ),
+            ],
+            rows: List<DataRow>.generate(
+                100,
+                    (index) => DataRow(cells: [
+                  DataCell(Center(child: Text('A' * (10 - index % 10)))),
+                  DataCell(Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(width: 50,child: Text('B' * (10 - (index + 5) % 10))),
+                        VerticalDivider(color: Colors.black87,thickness: 1),
+                        SizedBox(width: 50,child: Text('B' * (10 - (index + 5) % 10))),
+                        VerticalDivider(color: Colors.black87,thickness: 1),
+                        SizedBox(width: 50,child: Text('B' * (10 - (index + 5) % 10))),
+                      ],
+                    ),
+                  )),
+                  DataCell(Center(child: Text('C' * (10 - index % 10)))),
+                  DataCell(Center(child: Text('D' * (10 - index % 10)))),
+                ]))),
       ),
     );
   }
